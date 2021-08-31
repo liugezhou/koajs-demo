b4237e34b378eb32c1873d4f68beb243eca50640
liumingzhou <18231133236@163.com>
2021-08-31 21:15:18 +0800

    解析POST请求上下文中表单数据

--- post/index.js
+++ post/index.js
@@ -1,17 +1,50 @@
 const Koa = require('koa');
 const app = new Koa();
 const fs = require('fs');
+const path = require('path');
 // 获取GET请求有两个途径：ctx.query或者ctx.request.query
 app.use(async (ctx) => {
     if (ctx.url === '/' && ctx.method === 'GET') {
         const html = await render()
         ctx.body = html
     }
+    if (ctx.url === '/' && ctx.method === 'POST') {
+        const postData = await parsepostData(ctx)
+        ctx.body = postData
+    }
 })
 
+function parsepostData(ctx){
+    return new Promise((resolve,reject) => {
+        try {
+           let postData = ''
+           ctx.req.addListener('data',(data) => {
+               postData += data
+           }) 
+          ctx.req.addListener('end',()=>{
+            const res = parseQueryStr(postData)
+           resolve(res)
+          })
+           
+        } catch (error) {
+            console.log(error)
+            reject(error)
+        }
+    })
+}
+
+function parseQueryStr(data){
+    const arr = data.split('&')
+    let result = {};
+    arr.forEach(element => {
+        let str = element.split('=');
+        result[str[0]] = decodeURIComponent(str[1])
+    });
+    return result;
+}
 function render(){
     return new Promise((resolve,reject) =>{
-        let viewUrl = `../post/demo.html`
+        let viewUrl = path.join(__dirname,'./demo.html')
         fs.readFile(viewUrl, "binary", ( err, data ) => {
             if ( err ) {
               reject( err )
